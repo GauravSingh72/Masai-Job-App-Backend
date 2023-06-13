c2b974c60f1dffa369dc276c49d6dcdf36e7cdfc
GauravSingh72 <1gaurav2singh@gmail.com>
2023-06-13 12:29:21 +0530

    Backend Deployement

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { connectDB } = require("./config/db");
+const { JobRouter } = require("./routes/jobRoutes.routes");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+app.use("/jobPosts", JobRouter);
+
+app.listen(PORT, async (req, res) => {
+  try {
+    await connectDB;
+    console.log(`connected at port ${PORT}`);
+  } catch (error) {
+    console.log("connection failded", error);
+  }
+});
---
+++ models/job.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const jobSchema = new mongoose.Schema({
+  company: String,
+  postedAt: Date,
+  city: String,
+  location: String,
+  role: String,
+  level: String,
+  contract: String,
+  position: String,
+  language: String,
+});
+
+module.exports = mongoose.model("Job", jobSchema);
